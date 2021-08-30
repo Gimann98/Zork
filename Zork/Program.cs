0292cfb6c651ae4a6c6e937506060cf63ce8c338
Gianni Rodriguez <gianni.rodr@gmail.com>
2021-08-30 14:47:16 -0400

    Revert "Revert "Update Program.cs""
    This reverts commit fda30cd38b825a2a08a8e5302f5ccb282b2d4f3b.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,6 +7,20 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+            string inputstring = Console.ReadLine();
+            inputstring = inputstring.ToUpper();
+            if (inputstring == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing.");
+            }
+            else if (inputstring == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of the white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized command.");
+            }
         }
     }
 }
