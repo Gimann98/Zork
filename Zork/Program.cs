3ff22423aed38cb2c29fd1169a0d807372924aab
Gianni Rodriguez <gianni.rodr@gmail.com>
2021-09-08 02:45:59 -0400

    Zork 1.2 Finished

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -13,42 +13,6 @@ namespace Zork
             Console.WriteLine(command);
         }
 
-        private static Commands ToCommand(string commandString)
-        {
-            Commands command;
-
-            if (commandString =="QUIT")
-            {
-                command = Commands.QUIT;
-            }
-            else if (commandString == "LOOK")
-            {
-                command = Commands.LOOK;
-            }
-            else if (commandString == "NORTH")
-            {
-                command = Commands.NORTH;
-            }
-            else if (commandString == "SOUTH")
-            {
-                command = Commands.SOUTH;
-            }
-            else if (commandString == "EAST")
-            {
-                command = Commands.EAST;
-            }
-            else if (commandString == "WEST")
-            {
-                command = Commands.WEST;
-            }
-            else
-            {
-                command = Commands.UNKNOWN;
-            }
-
-            return command;
-            }
-        }
-         
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
 }
         
