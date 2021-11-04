ee02537023726db46af1ba6d8e4b6ef19a35a318
Gianni Rodriguez <gianni.rodr@gmail.com>
2021-11-04 17:58:11 -0400

    Zork Builder

---
+++ Zork.Common/DictionaryExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Zork.Common
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetValueOrDefaultKey<Tkey, TValue>(this IDictionary<Tkey, TValue> dictionary, Tkey key, TValue defaultValue = default(TValue))
+        {
+            return (dictionary != null && key != null && dictionary.TryGetValue(key, out TValue value)) ? value : defaultValue;
+        }
+    }
+}
