92a7f1393c08b1b529184d219a4ebd48d2f06606
Gianni Rodriguez <gianni.rodr@gmail.com>
2021-11-10 15:02:51 -0500

    Forgotten Changes

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -9,10 +9,14 @@ namespace Zork
             const string defaultGameFilename = "Zork.json";
             string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFileName] : defaultGameFilename);
 
-            ConsoleOutputService ouput = new ConsoleOutputService();
+            ConsoleOutputService output = new ConsoleOutputService();
+            ConsoleInputService input = new ConsoleInputService();
+
+            Game.Player.LocationChanged += Player_LocationChanged;
+
             Game game = Game.Load(gameFilename);
             Console.WriteLine("Welcome to Zork!");
-            game.Run(output);
+            game.Run(input, output);
             Console.WriteLine("Thank you for playing!");
         }
 
